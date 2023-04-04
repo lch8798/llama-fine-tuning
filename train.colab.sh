90397fe6cc53147d33a14bc005a29a132d579a6e
onCloud241 <33272151+lch8798@users.noreply.github.com>
2023-04-04 21:49:36 +0900

    add. default tokenizer config

--- train.colab.sh
+++ train.colab.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=1 --master_port=34321 train.py \
     --model_name_or_path "decapoda-research/llama-7b-hf" \
-    --tokenizer_path "./models/tokenizer.model" \
+    --tokenizer_path "./models" \
     --cache_dir "./models/decapoda-research/llama-7b-hf" \
     --data_path "./datasets/alpaca/ko.json" \
     --bf16 True \
